5f9045979809887728019ad75abb4394563ba105
Yatin T <yatint5@gmail.com>
2020-05-11 15:05:21 +0530

    reshape_images.py and get_image.py completed

--- get_image.py
+++ get_image.py
@@ -4,49 +4,64 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import StaleElementReferenceException
 
-driver = webdriver.Firefox(executable_path=r'D:\Installers\geckodriver.exe')
-driver.get('https://www.oliverboorman.biz/projects/tools/digital_clock.php')
+driver = webdriver.Firefox(executable_path=r'D:\Installers\geckodriver.exe') # path to geckodriver for firefox
 
 def click_on_element(x_path):
     element = driver.find_element_by_xpath(x_path)
     element.click()
     return element
 
-X_PATH_BGCOLOR = f'/html/body/main/section/fieldset[1]/label[2]/input'
+
+WEBSITE            =  'https://www.oliverboorman.biz/projects/tools/digital_clock.php'
+
+X_PATH_IMAGE       = f'//*[@id="canvas"]'
+X_PATH_DIGITS      = f'//*[@id="ui-id-3"]'
+X_PATH_WHITE       = f'/html/body/ul[1]/li[5]'
+X_PATH_BLACK       = f'/html/body/ul[1]/li[2]'
+X_PATH_BGCOLOR     = f'/html/body/main/section/fieldset[1]/label[2]/input'
+X_PATH_HOURS       = f'/html/body/main/section/fieldset[3]/div[1]/label[1]/input'
+X_PATH_MINUTES     = f'/html/body/main/section/fieldset[3]/div[1]/label[2]/input'
+X_PATH_SECONDS     = f'/html/body/main/section/fieldset[3]/div[1]/label[3]/input'
+X_PATH_TEXTCOLOR   = f'/html/body/main/section/fieldset[3]/div[2]/label[1]/input'
+CSS_SELECTOR_AM_PM = f"div.column:nth-child(2) > label:nth-child(2) > select:nth-child(2)"
+
+#open website
+driver.get(WEBSITE)
+
+# make background black
 bgcolor = click_on_element(X_PATH_BGCOLOR)
-X_PATH_BLACK = '/html/body/ul[1]/li[2]'
 _ = click_on_element(X_PATH_BLACK)
 
-X_PATH_TIME = f'//*[@id="ui-id-3"]'
-time_ele = click_on_element(X_PATH_TIME)
+# click on time tab
+time_ele = click_on_element(X_PATH_DIGITS)
 
-X_PATH_HOURS = '/html/body/main/section/fieldset[3]/div[1]/label[1]/input'
-hours_ele = click_on_element(X_PATH_HOURS)
-hours_ele.clear()
-hours_ele.send_keys('00')
-
-X_PATH_MINUTES = '/html/body/main/section/fieldset[3]/div[1]/label[2]/input'
-minutes_ele = click_on_element(X_PATH_MINUTES)
-minutes_ele.clear()
-minutes_ele.send_keys('00')
-
-X_PATH_SECONDS = '/html/body/main/section/fieldset[3]/div[1]/label[3]/input'
-seconds_ele = click_on_element(X_PATH_SECONDS)
-seconds_ele.clear()
-seconds_ele.send_keys('00')
-
-CSS_SELECTOR_AM_PM = "div.column:nth-child(2) > label:nth-child(2) > select:nth-child(2)"
+#set 24hr format
 am_pm_ele = Select(driver.find_element_by_css_selector(CSS_SELECTOR_AM_PM))
 am_pm_ele.select_by_index(0)
 
-X_PATH_TEXTCOLOR = f'/html/body/main/section/fieldset[3]/div[2]/label[1]/input'
+#make text color white
 textcolor = click_on_element(X_PATH_TEXTCOLOR)
-X_PATH_WHITE = '/html/body/ul[1]/li[5]'
 _ = click_on_element(X_PATH_WHITE)
 
+for hour in range(0,24,1):
+	for minute in range(0,59,1):
+		for second in range(0,59,10):
+			# set hours
+			hours_ele = click_on_element(X_PATH_HOURS)
+			hours_ele.clear()
+			hours_ele.send_keys(str(hour))
 
-X_PATH_IMAGE = '//*[@id="canvas"]'
+			#set minutes
+			minutes_ele = click_on_element(X_PATH_MINUTES)
+			minutes_ele.clear()
+			minutes_ele.send_keys(str(minute))
+
+			#set seconds
+			seconds_ele = click_on_element(X_PATH_SECONDS)
+			seconds_ele.clear()
+			seconds_ele.send_keys(str(second))
 
 
-with open('test.png', 'wb') as file:
-    file.write(driver.find_element_by_xpath(X_PATH_IMAGE).screenshot_as_png)
+			#save image
+			with open('assets/{}_{}_{}.png'.format(str(hour),str(minute),str(second)), 'wb') as file:
+			    file.write(driver.find_element_by_xpath(X_PATH_IMAGE).screenshot_as_png)
