d88f15880c358780ebea7b2a509973d626b5f591
Yatin T <yatint5@gmail.com>
2020-05-11 15:46:27 +0530

    reshape_image.py ran successfully

--- reshape_images.py
+++ reshape_images.py
@@ -9,15 +9,22 @@ RESHAPED = 'assets/reshaped'
 if not os.path.exists(RESHAPED):
 	os.mkdir(RESHAPED)
 
-for image in os.listdir(ASSETS)
-	image_path = os.path.join(ASSETS,image)
-	image = cv2.imread(image_path) 
-	scale_percent = 70
-	width = int(image.shape[1] * scale_percent / 100)
-	height = int(image.shape[0] * scale_percent / 100)
-	dim = (width, height)
+for file in os.listdir(ASSETS):
 
-	# resize image
-	image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA) 
-	image = cv2.copyMakeBorder(image, 100, 100, 105, 100, cv2.BORDER_CONSTANT) 
-	cv2.imwrite(os.path.join(RESHAPED,image),image)
+	_,ext = os.path.splitext(file)
+	if ext == '.png': # make sure it is an image
+		image_path = os.path.join(ASSETS,file)
+		image = cv2.imread(image_path) 
+
+		scale_percent = 70
+		width = int(image.shape[1] * scale_percent / 100)
+		height = int(image.shape[0] * scale_percent / 100)
+		dim = (width, height)
+
+		# resize image
+		image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA) 
+		image = cv2.copyMakeBorder(image, 100, 100, 105, 100, cv2.BORDER_CONSTANT) 
+		cv2.imwrite(os.path.join(RESHAPED,file),image)
+		print(file,'done')
+	else:
+		print('ignored',file)
